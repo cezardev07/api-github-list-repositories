c44372d2d6077ef1607ca22a19ad0ca842d92c66
cezardev07 <103595580+cezardev07@users.noreply.github.com>
2023-03-03 15:25:12 -0300

    feat: add script page

---
+++ js/app.js
@@ -0,0 +1,239 @@
+const url = "https://api.github.com/users/cezardev07/repos"
+const menu = document.querySelector(".repositorio")
+
+const urlRepo = "https://github.com/cezardev07/"
+
+const get = async function(){
+    const res = await fetch(url)
+    const db = await res.json()
+
+    return db
+}
+const user = async function(){
+    const db = await get()
+    
+    for(let i = 0; i < db.length; i++){
+        //console.log(db[i].name)
+
+        const repo = document.createElement("div")
+        repo.classList = "repo"
+
+        menu.appendChild(repo)
+        repo.innerHTML = `
+
+            <div class="title">
+                <a href="${urlRepo + db[i].name}" class="link-title"  target="_blank" rel="noopener noreferrer">
+                    ${db[i].name}
+                </a>
+                <span class="repo-visibility">
+                    ${db[i].visibility}
+                </span>
+            </div>
+            <div class="description">
+                <p>
+                    ${db[i].description}
+                </p>
+            </div>
+            <div class="language">
+                <div class="circle-language"></div>
+                <p>
+                    ${db[i].language}
+                </p>
+            </div>
+        
+        `
+    }
+
+    colorLanguage()
+    buscaRepo()
+
+}
+user()
+
+const buscaRepo = function(){
+    const inp = document.querySelector("input")
+    inp.oninput = filterRepositorio;
+
+    function filterRepositorio(){
+        const repositorio = document.querySelectorAll(".repo")
+        const valor = inp.value.toLocaleLowerCase()
+
+        repositorio.forEach(repo => {
+            // repo.children[0] 
+            if(valor === ""){
+                repo.classList.remove("closeRepositorio")
+                return
+            }
+            if(repo.textContent.includes(valor)){
+                repo.classList.remove("closeRepositorio")
+            }else{
+                repo.classList.add("closeRepositorio")
+            }
+
+        })
+
+    }
+}
+
+const colorLanguage = function(){
+    const languageRepo = document.querySelectorAll(".language")
+
+    languageRepo.forEach(item => {
+        const circle = item.children[0]
+
+        if(item.children[1].innerText === "CSS"){
+            circle.style.background = "#573d7c"
+            return
+        }
+        if(item.children[1].innerText === "HTML"){
+            circle.style.background = "#e34c26"
+            return
+        }
+        if(item.children[1].innerText === "JavaScript "){
+            circle.style.background = "#f1e05a"
+            return
+        }
+        if(item.children[1].innerText === "Python"){
+            circle.style.background = "#58a6ff"
+            return
+        }
+        if(item.children[1].innerText === "C" || item.children[1].innerText === "C++"){
+            circle.style.background = "#f34b7d"
+            return
+        }
+
+    })
+
+}
+
+const mainRepo = document.querySelector("main")
+const overviewRepo = document.querySelector(".overview")
+
+const btnOver = document.querySelector(".btn-over")
+const btnRep = document.querySelector(".btn-repo")
+
+btnOver.onclick = over;
+btnRep.onclick = rep;
+
+function over(){
+    overviewRepo.classList.remove("close-repo")
+    mainRepo.classList.add("close-repo")
+
+    colorLanguage()
+
+    btnRep.style.borderBottomColor = "transparent"
+    btnOver.style.borderBottomColor = "#f78166"
+}
+
+function rep(){
+    overviewRepo.classList.add("close-repo")
+    mainRepo.classList.remove("close-repo")
+
+    colorLanguage()
+
+    btnRep.style.borderBottomColor = "#f78166"
+    btnOver.style.borderBottomColor = "transparent"
+}
+
+const pinnedFixed = async function(){
+    const data = await get()
+    console.log(data)
+    overviewRepo.innerHTML = `
+    <h2 class="overview-title-pinned">Pinned</h2>
+
+    <div class="overview-repo">
+        <div class="title">
+            <a href="${urlRepo + data[8].name}" target="_blank" rel="noopener noreferrer"class="link-title">
+                ${data[8].name}
+            </a>
+            <span class="repo-visibility">
+                ${data[8].visibility}
+            </span>
+        </div>
+        <div class="description">
+            <p>
+                 ${data[8].description}
+            </p>
+        </div>
+        <div class="language">
+            <div class="overview-circle-language"></div>
+            <p> 
+              ${data[8].language}
+            </p>
+        </div>
+    </div>
+
+    <div class="overview-repo">
+        <div class="title">
+            <a href="${urlRepo + data[6].name}" target="_blank" rel="noopener noreferrer"class="link-title">
+                ${data[6].name}
+            </a>
+            <span class="repo-visibility">
+                ${data[6].visibility}
+            </span>
+        </div>
+        <div class="description">
+            <p>
+                 ${data[6].description}
+            </p>
+        </div>
+        <div class="language">
+            <div class="overview-circle-language"></div>
+            <p> 
+              ${data[6].language}
+            </p>
+        </div>
+    </div>
+
+
+    <div class="overview-repo">
+        <div class="title">
+            <a href="${urlRepo + data[12].name}" target="_blank" rel="noopener noreferrer"class="link-title">
+                ${data[12].name}
+            </a>
+            <span class="repo-visibility">
+                ${data[12].visibility}
+            </span>
+        </div>
+        <div class="description">
+            <p>
+                 ${data[12].description}
+            </p>
+        </div>
+        <div class="language">
+            <div class="overview-circle-language"></div>
+            <p> 
+              ${data[12].language}
+            </p>
+        </div>
+    </div>
+
+    
+    <div class="overview-repo">
+        <div class="title">
+            <a href="${urlRepo + data[14].name}" target="_blank" rel="noopener noreferrer"class="link-title">
+                ${data[14].name}
+            </a>
+            <span class="repo-visibility">
+                ${data[14].visibility}
+            </span>
+        </div>
+        <div class="description">
+            <p>
+                 ${data[14].description}
+            </p>
+        </div>
+        <div class="language">
+            <div class="overview-circle-language"></div>
+            <p> 
+              ${data[14].language}
+            </p>
+        </div>
+    </div>
+
+    `
+    colorLanguage()
+}
+pinnedFixed()
+
+console.log("Hello dev!")
